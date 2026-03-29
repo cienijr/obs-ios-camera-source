dceee77492a2900ab1f39ffae155b6944b9fcf42
José Carlos Cieni Júnior <cienijr@outlook.com>
2026-03-28 21:26:12 -0300

    Rename build directories from build32/build64 to build_x86/build_x64

--- CI/checkout-cmake-obs-windows.cmd
+++ CI/checkout-cmake-obs-windows.cmd
@@ -69,12 +69,12 @@ if not exist %OBSPath% (
 
 REM If the needed obs-studio libs for this build_config do not exist,
 REM set the build flag.
-if not exist %OBSPath%\build32\libobs\%build_config%\obs.lib (
-	echo obs-studio\build32\libobs\%build_config%\obs.lib does not exist
+if not exist %OBSPath%\build_x86\libobs\%build_config%\obs.lib (
+	echo obs-studio\build_x86\libobs\%build_config%\obs.lib does not exist
 	set BuildOBS=true
 )
-if not exist %OBSPath%\build32\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib (
-	echo obs-studio\build32\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib does not exist
+if not exist %OBSPath%\build_x86\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib (
+	echo obs-studio\build_x86\UI\obs-frontend-api\%build_config%\obs-frontend-api.lib does not exist
 	set BuildOBS=true
 )
 
@@ -101,30 +101,30 @@ if defined BuildOBS (
 	echo:
 	
     echo   Removing previous build dirs...
-	if exist build32 rmdir /s /q "%OBSPath%\build32"
-	if exist build64 rmdir /s /q "%OBSPath%\build64"
+	if exist build_x86 rmdir /s /q "%OBSPath%\build_x86"
+	if exist build_x64 rmdir /s /q "%OBSPath%\build_x64"
 	
     echo   Making new build dirs...
-	mkdir build32
-	mkdir build64
+	mkdir build_x86
+	mkdir build_x64
 	
     echo   Running cmake for obs-studio %OBSLatestTag% 32-bit...
-	cd build32
+	cd build_x86
 	cmake -G "Visual Studio 17 2022" -A Win32 -DCMAKE_SYSTEM_VERSION=10.0 -DQTDIR="%QTDIR32%" -DDepsPath="%DepsPath32%" -DBUILD_CAPTIONS=true -DDISABLE_PLUGINS=true -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true ..
 	echo:
 	echo:
 	
     echo   Running cmake for obs-studio %OBSLatestTag% 64-bit...
-	cd ..\build64
+	cd ..\build_x64
 	cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_SYSTEM_VERSION=10.0 -DQTDIR="%QTDIR64%" -DDepsPath="%DepsPath64%" -DBUILD_CAPTIONS=true -DDISABLE_PLUGINS=true -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true ..
 	echo:
 	echo:
 	
     echo   Building obs-studio %OBSLatestTag% 32-bit ^(Build Config: %build_config%^)...
-	call msbuild /m /p:Configuration=%build_config% %OBSPath%\build32\obs-studio.sln
+	call msbuild /m /p:Configuration=%build_config% %OBSPath%\build_x86\obs-studio.sln
 	
     echo   Building obs-studio %OBSLatestTag% 64-bit ^(Build Config: %build_config%^)...
-	call msbuild /m /p:Configuration=%build_config% %OBSPath%\build64\obs-studio.sln
+	call msbuild /m /p:Configuration=%build_config% %OBSPath%\build_x64\obs-studio.sln
 	
     cd ..
 	git describe --tags --abbrev=0 > "%OBSPath%\obs-studio-last-tag-built.txt"
